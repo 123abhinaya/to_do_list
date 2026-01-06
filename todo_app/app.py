23c776e1136afb493944ce5fa26c5ba9e341d464
Abhinaya Sri <abhinayasritaneeru@gmail.com>
2026-01-06 11:40:01 +0530

    initial commit

---
+++ todo_app/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3, time
+
+app = Flask(__name__)
+
+def get_db():
+    conn = sqlite3.connect("todo.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_seconds(value, unit):
+    if unit == "minutes":
+        return value * 60
+    if unit == "hours":
+        return value * 3600
+    if unit == "days":
+        return value * 86400
+    if unit == "months":
+        return value * 2592000
+
+
+@app.route('/')
+def index():
+    conn = get_db()
+    tasks = conn.execute("SELECT * FROM tasks").fetchall()
+    conn.close()
+
+    now = int(time.time())
+    updated_tasks = []
+
+    for t in tasks:
+        time_left = (t["created_at"] + get_seconds(t["deadline"], t["unit"])) - now
+
+        if t["status"] != "Completed":
+            if time_left <= 0:
+                status = "Overdue"
+            else:
+                status = "Pending"
+        else:
+            status = "Completed"
+
+        updated_tasks.append({
+            **dict(t),
+            "status": status,
+            "time_left": time_left
+        })
+
+    return render_template("index.html", tasks=updated_tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form['title']
+    deadline = int(request.form['deadline'])
+    unit = request.form['unit']
+    created_at = int(time.time())
+
+    conn = get_db()
+    conn.execute(
+        "INSERT INTO tasks (title, deadline, unit, created_at) VALUES (?,?,?,?)",
+        (title, deadline, unit, created_at)
+    )
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+@app.route('/complete/<int:id>')
+def complete(id):
+    conn = get_db()
+    conn.execute("UPDATE tasks SET status='Completed' WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    conn = get_db()
+    conn.execute("DELETE FROM tasks WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
